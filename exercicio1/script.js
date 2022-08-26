c688188fc82c4afda9335d398a3d263654c6be7b
marcusvrr <marcusvrr2@gmail.com>
2022-08-25 21:52:07 -0300

    exercicios corrigidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -7,13 +7,13 @@ const objeto = {
 
 //1
 function capsLock(elemento) {
-    const propriedadeAlta = {...objeto, nome: objeto.nome.toUpperCase(), profissao: objeto.profissao.toUpperCase(), username: objeto.username.toUpperCase(), senha: objeto.senha.toUpperCase()}
+    const propriedadeAlta = {...objeto, nome: elemento.nome.toUpperCase(), profissao: elemento.profissao.toUpperCase(), username: elemento.username.toUpperCase(), senha: elemento.senha.toUpperCase()}
     return propriedadeAlta
 }
 
 //2
 function textoCorrido(elemento) {
-    const propriedadeAlta = {...objeto, textoCorrido: objeto.nome+ objeto.profissao+ objeto.username + objeto.senha }
+    const propriedadeAlta = {...objeto, textoCorrido: elemento.nome + elemento.profissao + elemento.username + elemento.senha }
     return propriedadeAlta.textoCorrido
 }
 
@@ -25,3 +25,28 @@ function funcao3(elemento,callback) {
 }
 funcao3(objeto,capsLock);
 funcao3(objeto,textoCorrido);
+
+//exercicio paula ex.exer1
+const pessoa = {
+    nome:"Paula",
+    Profissão:"Desenvolvedora",
+    username:"paulaOliveira",
+    senha: "123456"
+}
+function recebeObj1(objeto){
+    let novoObjeto={}
+    for(let i in objeto){
+        novoObjeto[i]=objeto[i].toUpperCase()
+    }
+    return novoObjeto
+}
+console.log(recebeObj1(pessoa));
+
+const recebeObj2 = (objeto)=>{
+    let textoNovo =[]
+    for(let texto in objeto){
+        textoNovo.push(objeto[texto])
+    }
+    return textoNovo.toString().replaceAll(",", " ")
+}
+console.log(recebeObj2(pessoa))
